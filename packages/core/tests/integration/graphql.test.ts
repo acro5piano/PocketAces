dc8df4c9ba72206b84ef3b8fb8c11f27ab1df048
gosho-kazuya <ketsume0211@gmail.com>
2020-05-12 01:00:24 +0900

    fix tsc

--- packages/core/tests/integration/graphql.test.ts
+++ packages/core/tests/integration/graphql.test.ts
@@ -2,12 +2,6 @@ import { test, gql } from '../helper'
 import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql'
 import graphql from '../../src/services/GraphQLService'
 
-const schema = gql`
-  type Query {
-    health: String!
-  }
-`
-
 test('graphql#health', async t => {
   graphql.schema = new GraphQLSchema({
     query: new GraphQLObjectType({
