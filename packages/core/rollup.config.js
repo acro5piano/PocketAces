45d626b6ce3d3e92f092c5e88ba32b724832fee9
gosho-kazuya <ketsume0211@gmail.com>
2020-05-13 00:05:07 +0900

    prepare for npm

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -9,9 +9,14 @@ export default {
   external: id => !id.startsWith('.') && !id.startsWith('/') && id !== 'tslib' && id !== 'refrect-metadata',
   output: [
     {
-      dir: 'build',
+      file: 'build/index.js',
       format: 'cjs',
       name: 'pocket-ace',
+      sourcemap: true,
+    },
+    {
+      file: 'build/index.es.js',
+      format: 'es',
       sourcemap: true,
     },
   ],
