62cfb6cf65c072172b0c2b4f65f3727063212dbd
gosho-kazuya <ketsume0211@gmail.com>
2020-06-18 01:19:30 +0900

    add comment

--- packages/core/src/directives/HasManyDirective.ts
+++ packages/core/src/directives/HasManyDirective.ts
@@ -1,34 +1,7 @@
-// import { BaseDirective } from './BaseDirective'
-// import { DirectiveExecutionArgs } from 'src/contracts/DirectiveContract'
-// import { typeToTable, joinKeyFromTable } from 'src/database/Convension'
-//
-// export class HasManyDirective extends BaseDirective<{
-//   table?: string
-//   joins: string
-// }> {
-//   name = 'hasMany'
-//
-//   async resolveField({
-//     parentValue,
-//     resolveInfo,
-//   }: DirectiveExecutionArgs<any, { id: string }>) {
-//     const table = typeToTable(
-//       this.getDirectiveArgValue('table'),
-//       resolveInfo.returnType,
-//     )
-//
-//     const joinKey =
-//       this.getDirectiveArgValue('joins') ||
-//       joinKeyFromTable(this.getParentTypeName())
-//
-//     const rows = this.loader.getLoader(table, joinKey).load(parentValue.id)
-//     return rows
-//   }
-// }
-
 import { joinKeyFromTable } from 'src/database/Convension'
 import { DirectiveProps } from 'src/contracts/DirectiveContract'
 
+// TODO: currently happening N+1 problem!
 export default function hasMany({
   inferredTableName,
   parentValue,
